67a4e2349c69c66e36840fdee191acf0c58de0d3
Pavankumar DK Karekenchappanavar <pkareken@in.ibm.com>
2020-02-11 22:21:27 +0530

    variable conversion code from C++ to Python

--- Test.py
+++ Test.py
@@ -1,1 +1,28 @@
-Test Line
+import re
+
+''' this program is used to convert variable from C++ to python and Python to C++'''
+class name():
+
+    def str():
+        
+        name = input('enter a string\n')
+        
+ 
+        print ("entered variale in C++ is")
+        name = name.replace(" ","-")
+        name = name.replace("_","-")
+        print(name)
+
+        name = input('enter a string\n')
+        
+        print ("entered variale in python is")
+        name = name.replace(" ","_")
+        name = name.replace("-","_")
+        print(name)
+
+        
+###test = pavan()
+##name = 'vari_able'
+print(name.str())
+
+        
